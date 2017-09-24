75c98844dd46e79a11f51899403a7038396bccbc
connie10 <32081636+connie10@users.noreply.github.com>
2017-09-24 13:32:37 +1000

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// load the DOM
+
+$(document).ready(function(){
+
+  //Log
+  console.log('ready!');
+
+  //Click the hamburger icon
+  $('.toggle-mobile-menu').click(function(){
+
+    //Show the mobile menu
+    $('#mobile-menu').toggleClass('mobile-menu-visible');
+
+  });
+  //Clicking the cross icon
+  //Close the mobile menu
+
+});
